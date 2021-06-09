97f6817da16d3b7a9ffc2a8b7db4cf2f27a1898e
richardcwalker <rwalkerwebdesign@gmail.com>
2021-06-09 15:17:41 +0100

    Add empty unit test project (build issues?!?!?)

---
+++ CheckOutScannertests/ItemModelTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace CheckOutScannertests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
